0145faeecf6c6030146efbf3bb02907a5a5aa8ab
seg24nxn <seg24nxn-github@sebastianguenther.me>
2026-09-19 20:55:31 +0200

    cleanud up the code, added main and other fuctions

--- tesstest.py
+++ tesstest.py
@@ -1,21 +1,61 @@
 import pytesseract
 from PIL import Image, ImageOps
 
-# image = Image.open("test_images/handwritten_sample_easy.png")
-# image = Image.open("test_images/printed_sample.png")
-image = Image.open("test_images/10 random aresses PNG/page-10.png")
 
-# processing for better accuracy
-# image = image.crop((280, 50, 800, 1050))
-# image = image.rotate(-90, expand=True)
-image = ImageOps.grayscale(image)
-image = image.resize((image.width * 3, image.height * 3))
-image = ImageOps.autocontrast(image)
-image = image.point(lambda p: 255 if p > 160 else 0)
+def main():
+    # image = Image.open("test_images/handwritten_sample_easy.png")
+    image = Image.open("test_images/printed_sample.png")
+    # image = Image.open("test_images/10 random aresses PNG/page-10.png")
+    image = imageCutting(image)
+    image = imageProcessing(image)
+    testOutput(image)
+    text = imageToText(image)
+    processText(text)
+    printText(text)
 
-# check how the image looks
-image.save("output_image/debug_output.png")
 
-# convertion
-text = pytesseract.image_to_string(image)
-print(repr(text))
+def imageCutting(image):
+    # TODO: implement
+    # processing for better accuracy
+    image = image.crop((280, 50, 800, 1050))
+    image = image.rotate(-90, expand=True)
+    print("Cutting Image...")
+    return image
+
+
+def imageProcessing(image):
+    image = ImageOps.grayscale(image)
+    image = image.resize((image.width * 3, image.height * 3))
+    image = ImageOps.autocontrast(image)
+    image = image.point(lambda p: 255 if p > 160 else 0)
+    print("Processing Image...")
+    return image
+
+
+def testOutput(image):
+    # check how the processed image looks
+    image.save("output_image/debug_output.png")
+    print("Saving Processed Image...")
+    return
+
+
+def imageToText(image):
+    # the actual converting of the image into text
+    text = pytesseract.image_to_string(image)
+    print("Converting Image to Text...")
+    return text
+
+
+def processText(text):
+    # TODO: later for decision making about output
+    print("Processing Text...")
+    return
+
+
+def printText(text):
+    print(repr(text))
+    return
+
+
+if __name__ == "__main__":
+    main()
